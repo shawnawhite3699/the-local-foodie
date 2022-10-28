d22856547ef685909b3504102c78f0fe703ae45c
James Carter <carterjamesmike@gmail.com>
2022-10-27 20:29:00 -0500

    Function to handle all buttons and call further api functions created

--- assets/js/googleAPI.js
+++ assets/js/googleAPI.js
@@ -1,40 +1,52 @@
-getGeocode();
+//'rests' is a placeholder. Will need restaurants in a specific div with id
+var rests = document.getElementById('rests');
+console.log(rests);
 
-function initMap(userLat, userLon) {
-    // The location of the restaurant
+function initMap() {
+    // Lat and lon of restaurant pulled from getGeocode fx
     const local = { lat: userLat, lng: userLon};
     // Centers map on restaurant
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 16,
       center: local,
     });
-    // places marker on restaurant
+    // Positions marker on restaurant
     const marker = new google.maps.Marker({
       position: local,
       map: map,
     });
   }
   
-  window.initMap = initMap;
+window.initMap = initMap;
 
-  function getGeocode () {
+function getGeocode (btnAttribute) {
+  //Line below will be used once buttons are coded in. Line two below is a placeholder for design layout
+  //var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${btnAttribute}&key=AIzaSyDVM4nCzUY3gFGHGXhyDnzXz8ZZbcT0e1w`;
+  var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=862487R6%2BGM&key=AIzaSyDVM4nCzUY3gFGHGXhyDnzXz8ZZbcT0e1w`;
 
-    var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=8624874G%2BV2&key=AIzaSyDVM4nCzUY3gFGHGXhyDnzXz8ZZbcT0e1w`;
 
-    fetch(geocodeUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data);
-            // console.log(data.results[0].geometry.location.lat);
-            // console.log(data.results[0].geometry.location.lng);
-            var userLat = data.results[0].geometry.location.lat;
-            var userLon = data.results[0].geometry.location.lng;
-            // console.log(userLat);
-            // console.log(userLon);
-            initMap(userLat, userLon)
-        });
+  fetch(geocodeUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      // console.log(data.results[0].geometry.location.lat);
+      // console.log(data.results[0].geometry.location.lng);
+      userLat = data.results[0].geometry.location.lat;
+      userLon = data.results[0].geometry.location.lng;
+      console.log(userLat);
+      console.log(userLon);
+      initMap(userLat, userLon)
+    });
 }
 
-//Still requires fx to add user input (Plus Code) to getGeocode fx which includes changing all "+" in plus code to %2B
+rests.addEventListener('click', function(event) {
+  var element = event.target;
+
+  if (element.matches('button') === true) {
+    var btnAttribute = element.getAttribute('id');
+    getGeocode(btnAttribute);
+  }
+})
+
